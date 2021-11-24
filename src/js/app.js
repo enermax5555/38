091c5ba8f55a2aecf5d1d2fa6cfa16d7bf5f783e
Стоян Калпакчиев <stoqn.sisimen@gmail.com>
2021-11-24 13:02:09 -0300

    Removed Ramda from package.json, Fix ramda syntax with js in app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,15 @@
 import "../scss/app.scss";
-const R = require("ramda");
+
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
   const ul = document.querySelector("ul");
   const numbersArray = [1, 3, 4, 5, 6, 7];
   const isEven = (n) => n % 2 === 0;
-  const filteredArray = R.filter(isEven, numbersArray);
+  const filteredArray = numbersArray.filter(isEven);
+  filteredArray.forEach((element) => {
+	  let setList = document.createElement("li");
+	  setList.innerText = element;
+	  ul.appendChild(setList);
+  })
 });
